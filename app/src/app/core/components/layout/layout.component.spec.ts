ec9f676c535f44cb6471dbcfc34e3307df738c78
Denis Wirries <denis@wirries.me>
2020-01-26 19:40:00 +0100

    fix test

--- app/src/app/core/components/layout/layout.component.spec.ts
+++ app/src/app/core/components/layout/layout.component.spec.ts
@@ -1,4 +1,6 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import {LayoutComponent} from './layout.component';
 
@@ -8,7 +10,9 @@ describe('LayoutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LayoutComponent]
+      declarations: [LayoutComponent],
+      imports: [RouterTestingModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
       .compileComponents();
   }));
