e3982bb6270d952b6edfc33b09f033d44e89ceda
Denis Wirries <denis@wirries.me>
2020-01-26 10:31:48 +0100

    living background

--- app/src/app/core/core.module.ts
+++ app/src/app/core/core.module.ts
@@ -14,6 +14,7 @@ import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {LoginComponent} from './components/login/login.component';
 import {LoggingService} from './service/logging.service';
+import {BackgroundComponent} from './components/background/background.component';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import {LoggingService} from './service/logging.service';
     LayoutComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    BackgroundComponent
   ],
   imports: [
     CommonModule,
