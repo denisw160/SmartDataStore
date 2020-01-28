9b92b67749ae7bd37f38e83189db4636d1b23a66
Denis Wirries <denis@wirries.me>
2020-01-28 21:19:20 +0100

    fix tests

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -1,16 +1,20 @@
 import {async, TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
+      imports: [
+        RouterTestingModule
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ]
     }).compileComponents();
   }));
 
@@ -20,16 +24,19 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'smartdatastore-app'`, () => {
+  it(`should have as title 'SmartDataStore'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('smartdatastore-app');
+    expect(app.title).toEqual('SmartDataStore');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('smartdatastore-app app is running!');
-  });
+  // This test is valid for the current design
+  // TODO Validate if header, content and footer is present
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('smartdatastore-app app is running!');
+  // });
+
 });
